22e0b7f8913ad43fee50f002a44535027f40005a
Irina Illustrova <illustrova@gmail.com>
2021-06-03 10:03:05 +0300

    fix add-component script

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -1,17 +1,17 @@
 /*
  * This is an index file for your library.
  * It's being updated automatically by add-component script
- * Don't edit it directly, your chages will be overwritten.
- */
+ * You can add tokens/components reexport, it will be preserved at next run, but not anything else.
+s */
 
 export { default as GlobalStyle } from "./global.js";
-export { colors, typography, spacing } from "./tokens";
+export { colors, typography } from "./tokens";
 
 export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Carousel } from "./components/Carousel";
 export { default as Icon } from "./components/Icon";
+export { default as Navigation } from "./components/Navigation";
+export { default as ProductItem } from "./components/ProductItem";
 export { default as Select } from "./components/Select";
 export { default as SummaryTable } from "./components/SummaryTable";
-export { default as ProductItem } from "./components/ProductItem";
-export { default as Navigation } from "./components/Navigation";
